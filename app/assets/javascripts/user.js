589c8dec5a819c8c6df4cfae476b334de4e49e1b
Yuta Osaka <yusoac@gmail.com>
2019-03-17 18:41:57 +0900

    不要な箇所を削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -17,7 +17,7 @@ $(document).on("turbolinks:load", function() {
 
   var members = $("#chat-group-users")
   function addUser(id, name) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-member'>
                 <input name='group[user_ids][]' type='hidden' value=${ id }>
                 <p class='chat-group-user__name'>${ name }</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
